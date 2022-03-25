a69bd095d5ed9f96af3a38da24363197564939d7
Kaushik Dey <Kaushikdey647@gmail.com>
2022-03-25 22:38:31 +0530

    feat(source)!: Created a new hello world

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CPP-Hello-World
 A Hello World in C++
+
+- The First File in the Series, added to demo how tf git works
---
+++ hello1.cpp
@@ -0,0 +1,107 @@
+/*
+ /$$                                 /$$                      /$$$$$$  /$$   /$$ /$$$$$$$$
+| $$                                | $$                     /$$__  $$| $$  | $$|_____ $$/
+| $$   /$$  /$$$$$$  /$$   /$$  /$$$$$$$  /$$$$$$   /$$$$$$ | $$  \__/| $$  | $$     /$$/ 
+| $$  /$$/ |____  $$| $$  | $$ /$$__  $$ /$$__  $$ /$$__  $$| $$$$$$$ | $$$$$$$$    /$$/  
+| $$$$$$/   /$$$$$$$| $$  | $$| $$  | $$| $$$$$$$$| $$$$$$$$| $$__  $$|_____  $$   /$$/   
+| $$_  $$  /$$__  $$| $$  | $$| $$  | $$| $$_____/| $$_____/| $$  \ $$      | $$  /$$/    
+| $$ \  $$|  $$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$/      | $$ /$$/     
+|__/  \__/ \_______/ \____  $$ \_______/ \_______/ \_______/ \______/       |__/|__/      
+                     /$$  | $$                                                            
+                    |  $$$$$$/                                                            
+                     \______/                                                             
+
+
+
+Name: Kaushik Dey
+Institute: IIT Bhubaneswar
+Mail: 20cs01043@iitbbs.ac.in
+Hackerrank: https://www.hackerrank.com/KayDee647
+Linkedin: https://www.linkedin.com/in/kaushik-dey-235531201/
+
+
+    "Everything should be made as simple as possible, But not simpler"	
+
+
+                                                                 - Anonymous
+*/
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>  
+#include <complex>
+#include <unordered_set>
+#include <list>
+#include <queue>
+#include <set>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+typedef unsigned long long int uli;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<uli> vu;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define f(i,s,e) for(ll i = s; i < e; i++)
+#define fr(i,s,e) for(ll i = s; i >= e; i--)
+#define v32f(V,i) for(v32::iterator i = V.begin();i != V.end();i++)
+#define v64f(V,i) for(v64::iterator i = V.begin();i != V.end();i++)
+#define vuf(V,i) for(vu::iterator i = V.begin();i != V.end();i++)
+#define v32fr(V,i) for(v32::iterator i = V.end();i != V.begin();i--)
+#define v64fr(V,i) for(v64::iterator i = V.end();i != V.begin();i--)
+#define vufr(V,i) for(vu::iterator i = V.end();i != V.begin();i--)
+#define cr "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+template<class T>
+T cinget(std::istream& is){
+  T result;
+  is >> result;
+  return result;
+}
+
+template<class T>
+void swap(T*a, T*b){
+T temp = *b;
+*b = *a;
+*a = temp;
+}
+
+void solve(){
+}
+int main()
+{
+    cout << "Hello World"
+}
